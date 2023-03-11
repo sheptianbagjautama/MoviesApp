86e579809ec90a03f79d62b2619f4254355c9fcb
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-11 12:13:32 +0700

    Create Movies Data Service

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
+import axios from 'axios';
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
+
+const getPopularMovies = async () => {
+     const resp = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=1a2b7b7ef669300f9fca253333e4b01b');
+     //Agar terlihat sama seperti response di postman
+     console.log(JSON.stringify(resp.data.results[0], null, 2));
+};
 
 const App = () => {
+  getPopularMovies();
+
   return (
     <View
       style={{
