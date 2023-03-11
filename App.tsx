021d14e6c3648aa3eddba7599db92e53f1d91c03
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-11 12:27:01 +0700

    Display Movie Data

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 
 const getPopularMovies = async () => {
-     const resp = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=1a2b7b7ef669300f9fca253333e4b01b');
-     //Agar terlihat sama seperti response di postman
-     console.log(JSON.stringify(resp.data.results[0], null, 2));
+  const resp = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=1a2b7b7ef669300f9fca253333e4b01b');
+  //NOTED : Agar terlihat sama seperti response di postman
+  //  console.log(JSON.stringify(resp.data.results[0], null, 2));
+  
+  return resp.data.results;
 };
 
 const App = () => {
-  getPopularMovies();
+  const [movie, setMovie] = useState('');
+  getPopularMovies().then(movies => {
+    setMovie(movies[0]); 
+  });
 
   return (
     <View
@@ -18,7 +23,9 @@ const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Movie Name : { movie.original_title }</Text>
+      <Text>Language :  { movie.original_language }</Text>
+      <Text>Release Date :  { movie.release_date }</Text>
     </View>
   );
 };
