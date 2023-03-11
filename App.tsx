b30a7a73c73bdd2174bfdc37402ace95d940fb8e
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-12 00:55:36 +0700

    Creating Home Screen Component

--- App.tsx
+++ App.tsx
@@ -1,34 +1,17 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { getPopularMovies } from './services/services';
-
+import React from 'react';
+import { View } from 'react-native';
+import Home from './screen/Home';
 
 const App = () => {
-  const [movie, setMovie] = useState('');
-  const [error, setError] = useState(false)
-  
-  useEffect(() => {
-    getPopularMovies().then(movies => {
-      setMovie(movies[0]); 
-    }).catch(err => {
-      setError(err);
-    });
-  }, [])
-  
-
   return (
-    <View
+  <View
       style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Movie Name : { movie.original_title }</Text>
-      <Text>Language :  { movie.original_language }</Text>
-      <Text>Release Date :  { movie.release_date }</Text>
-      {error && <Text style={{ color:'red' }}>Error in the server</Text>}
-    </View>
+        <Home/>
+      </View>
   );
 };
 
---
+++ screen/Home.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import {Text, View} from 'react-native';
+import {getPopularMovies} from '../services/services';
+
+const Home = () => {
+  const [movie, setMovie] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPopularMovies()
+      .then(movies => {
+        setMovie(movies[0]);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Movie Name : {movie.original_title}</Text>
+      <Text>Language : {movie.original_language}</Text>
+      <Text>Release Date : {movie.release_date}</Text>
+      {error && <Text style={{color: 'red'}}>Error in the server</Text>}
+    </View>
+  );
+};
+
+export default Home;
