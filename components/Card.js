b745294df1c677dfaf2019b2a67b96ae31b56730
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-13 09:33:28 +0700

    Image Placeholder in Card Component

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const placeholderImage = require('../assets/images/placeholder.png');
 
 class Card extends React.PureComponent {
   render() {
@@ -9,8 +11,16 @@ class Card extends React.PureComponent {
         <Image
           resizeMode="cover"
           style={styles.image}
-          source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}
+          //  source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}
+          source={
+            item.poster_path
+              ? {uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}
+              : placeholderImage
+          }
         />
+        {!item.poster_path && (
+          <Text style={styles.movieName}>{item.title}</Text>
+        )}
       </TouchableOpacity>
     );
   }
@@ -20,11 +30,18 @@ const styles = StyleSheet.create({
   container: {
     padding: 5,
     position: 'relative',
+    alignItems: 'center',
+    height: 200,
   },
   image: {
     height: 200,
     width: 120,
     borderRadius: 20,
+  },
+  movieName: {
+    position: 'absolute',
+    top: 10,
+    textAlign: 'center',
   },
 });
 
