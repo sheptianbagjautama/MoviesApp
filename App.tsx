6a657422c4d97e6f0cf7bff515f87fc99d994c79
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-18 02:37:25 +0700

    Creating a Stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screen/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-  <View
-      style={styles.container}>
-        <Home/>
-      </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-  }
-})
 
 export default App;
