59e92d464cdc785cc135026f8b4de82a249cbbc0
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-21 00:11:00 +0700

    Search Form

--- screen/Search.js
+++ screen/Search.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Search = ({navigation}) => {
+  const [text, onChangeText] = useState();
+
+  const onSubmit = query => {
+    console.log(query);
+  };
   return (
     <React.Fragment>
       <SafeAreaView>
-        <View>
-          <Text>{'Search'}</Text>
+        <View style={styles.container}>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholder={'Search Movie or TV Show'}
+              onChangeText={onChangeText}
+              value={text}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              onSubmit(text);
+            }}>
+            <Icon name={'search-outline'} size={30} />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 15,
+    borderWidth: 0.5,
+    height: 50,
+    padding: 8,
+  },
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+    paddingTop: 60,
+    alignItems: 'center',
+  },
+  form: {
+    flexBasis: 'auto',
+    flexGrow: 1,
+    paddingRight: 8,
+  },
+});
 
 export default Search;
